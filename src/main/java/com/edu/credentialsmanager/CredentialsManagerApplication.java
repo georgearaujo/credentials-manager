4b8792eb062d0237049d00bffd5843e28885251f
George Souza <george_souza97@outlook.com>
2021-08-02 01:25:47 -0300

    Adding structure default

---
+++ src/main/java/com/edu/credentialsmanager/CredentialsManagerApplication.java
@@ -0,0 +1,12 @@
+package com.edu.credentialsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredentialsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredentialsManagerApplication.class, args);
+	}
+}
